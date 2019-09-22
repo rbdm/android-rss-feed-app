c523420dccf7503c2cd717c80d97b38532172289
u6527600 <u6527600@anu.edu.au>
2019-09-23 00:53:45 +1000

    some basic change

--- app/src/main/java/com/feedreader/myapplication/MainActivity.java
+++ app/src/main/java/com/feedreader/myapplication/MainActivity.java
@@ -2,12 +2,20 @@ package com.feedreader.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
+    }
+
+
+    public void addSite(View v){
+        TextView ds=(TextView) findViewById(R.id.textView);
+        ds.setText("Discover new site:");
     }
 }
