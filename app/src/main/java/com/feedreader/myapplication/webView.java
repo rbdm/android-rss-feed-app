76728c758003c190e843bf0c87ed82f5dcbcfa07
Mirhady Dorodjatun <u6474009@anu.edu.au>
2019-10-04 06:01:27 +1000

    fixes home button and add standard share button

--- app/src/main/java/com/feedreader/myapplication/webView.java
+++ app/src/main/java/com/feedreader/myapplication/webView.java
@@ -3,11 +3,19 @@ package com.feedreader.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class webView  extends AppCompatActivity {
+
+    ImageButton buttonShare;
+    Button buttonHome;
+    Intent shareIntent, homeIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +34,25 @@ public class webView  extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
+            }
+        });
+
+        buttonShare = findViewById(R.id.buttonShare);
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                startActivity(Intent.createChooser(shareIntent, "Share"));
+            }
+        });
+
+        buttonHome = findViewById(R.id.buttonHome);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homeIntent = new Intent(webView.this, MainActivity.class);
+                webView.this.startActivity(homeIntent);
             }
         });
 
